91ecd2524228b893f400e57480720bb7cd41a258
Harlequin Doyon <harlequin.doyon@gmail.com>
2018-09-18 12:03:14 +0800

    Initial commit

---
+++ public/precache-manifest.93b89b39714dcd9f9a2b136e30a34b6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c862f565a4e19b7aec38",
+    "url": "/js/chunk-vendors.59645627.js"
+  },
+  {
+    "revision": "7c15282a3793e74ac2c8",
+    "url": "/js/app.9e3c54c6.js"
+  },
+  {
+    "revision": "6249ec0a09fa811f3d25",
+    "url": "/js/about.2ca2c031.js"
+  },
+  {
+    "revision": "92da5ea528df3959ce25656b27afeefc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7c15282a3793e74ac2c8",
+    "url": "/css/app.9615c6dc.css"
+  }
+];
