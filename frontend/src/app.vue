478f3da909a1f9bac21c916cf96b91b9ea00470f
Harlequin Doyon <harlequin.doyon@gmail.com>
2018-09-19 00:11:24 +0800

    Added the dashboard UI

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -45,44 +45,6 @@ export default {
   box-sizing: border-box;
 }
 
-body {
-  background: $color-body-bg;
-}
-
-#app {
-  @extend %typography-small;
-}
-
-// ===
-// Base element styles
-// ===
-
-a,
-a:visited {
-  color: $color-link-text;
-}
-
-h1 {
-  @extend %typography-xxlarge;
-}
-
-h2 {
-  @extend %typography-xlarge;
-}
-
-h3 {
-  @extend %typography-large;
-}
-
-h4 {
-  @extend %typography-medium;
-}
-
-h5,
-h6 {
-  @extend %typography-small;
-}
-
 // ===
 // Vendor
 // ===
