6fb442d380e28cd0a6dd246cf456e6237377162e
Harlequin Doyon <harlequin.doyon@gmail.com>
2018-09-19 15:20:40 +0800

    Updated table border

--- frontend/src/router/views/home.vue
+++ frontend/src/router/views/home.vue
@@ -36,7 +36,7 @@
 
       <table class="flex flex-col mb-12">
         <thead>
-          <th class="flex justify-around text-left border-b">
+          <th class="flex justify-around text-left border-b border-grey-lighter">
             <td class="flex-1 py-4">ID</td>
             <td class="flex-1 py-4">Supplier</td>
             <td class="flex-1 py-4">Created</td>
@@ -45,21 +45,21 @@
           </th>
         </thead>
         <tbody>
-          <tr class="flex justify-around border-b">
+          <tr class="flex justify-around border-b border-grey-lighter">
             <td class="flex-1 py-4">15324</td>
             <td class="flex-1 py-4">Ngo, Jacinto</td>
             <td class="flex-1 py-4">08/10/2018</td>
             <td class="flex-1 py-4">Somewhere</td>
             <td class="flex-1 py-4"><span class="uppercase bg-green text-white rounded py-1 px-4 text-xs font-light">Delivered</span></td>
           </tr>
-          <tr class="flex justify-around border-b">
+          <tr class="flex justify-around border-b border-grey-lighter">
             <td class="flex-1 py-4">15324</td>
             <td class="flex-1 py-4">Ngo, Jacinto</td>
             <td class="flex-1 py-4">08/10/2018</td>
             <td class="flex-1 py-4">Somewhere</td>
             <td class="flex-1 py-4"><span class="uppercase bg-purple-light text-white rounded py-1 px-4 text-xs font-light">Pending</span></td>
           </tr>
-          <tr class="flex justify-around border-b">
+          <tr class="flex justify-around border-b border-grey-lighter">
             <td class="flex-1 py-4">15324</td>
             <td class="flex-1 py-4">Ngo, Jacinto</td>
             <td class="flex-1 py-4">08/10/2018</td>
