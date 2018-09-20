edd97240fb90d6f7b2b417caaa4179cd3b84f962
Harlequin Doyon <harlequin.doyon@gmail.com>
2018-09-20 14:27:04 +0800

    Enhance editor UI

--- frontend/src/components/editor-sidebar.vue
+++ frontend/src/components/editor-sidebar.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="w-24 border-l border-grey-lighter absolute pin-t pin-r h-screen">
+  <div class="w-20 border-l border-grey-lighter absolute pin-t pin-r h-screen">
     <div class="flex flex-col text-center justify-between h-screen">
       <div class="flex flex-col">
-        <a class="flex items-center hover:text-blue-light justify-center h-24" href="#"><i class="icon-save"></i></a>
-        <a class="flex items-center hover:text-blue-light justify-center h-24" href="#"><i class="icon-info"></i></a>
-        <a class="flex items-center hover:text-blue-light justify-center h-24" href="#"><i class="icon-calculator"></i></a>
+        <a class="hover:text-blue-light" href="#">
+          <i class="icon-save"></i>
+        </a>
+        <a class="hover:text-blue-light" href="#">
+          <i class="icon-info"></i>
+        </a>
+        <a class="hover:text-blue-light" href="#">
+          <i class="icon-calculator"></i>
+        </a>
       </div>
       <div class="flex flex-col">
-        <a class="flex items-center hover:text-blue-light justify-center h-24" href="#"><i class="icon-erase"></i></a>
-        <a class="flex items-center hover:text-blue-light justify-center h-24" href="#"><i class="icon-print"></i></a>
-        <a class="flex items-center text-red-light hover:text-blue-light justify-center h-24 border-t border-grey-lighter" href="#"><i class="icon-power"></i></a>
+        <a class="hover:text-blue-light" href="#">
+          <i class="icon-erase"></i>
+        </a>
+        <a class="hover:text-blue-light" href="#">
+          <i class="icon-print"></i>
+        </a>
+        <a class="text-red-light hover:text-blue-light border-t border-grey-lighter" href="#">
+          <i class="icon-power"></i>
+        </a>
       </div>
     </div>
   </div>
@@ -23,6 +35,10 @@ export default {
 
 <style lang="postcss" scoped>
 i {
-  @apply text-3xl;
+  @apply text-2xl;
+}
+
+a {
+  @apply flex items-center justify-center h-20;
 }
 </style>
--- frontend/src/router/layouts/editor.vue
+++ frontend/src/router/layouts/editor.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative">
     <!-- NavBar -->
-    <div class="w-full absolute pin-t flex flex-col sm:flex-row sm:items-center justify-center sm:justify-start h-24 border-b border-grey-lighter">
+    <div class="w-full absolute pin-t flex flex-col sm:flex-row sm:items-center justify-center sm:justify-start h-20 border-b border-grey-lighter">
       <h1 class="text-3xl sm:text-4xl text-grey-darkest pl-2 sm:pl-8">Trajano, Cesar Jose</h1>
       <span class="text-lg ml-2 sm:ml-4 font-bold text-grey">(25 August 2018)</span>
     </div>
@@ -10,7 +10,7 @@
     <SideBar/>
 
     <!-- Content -->
-    <div class="mr-24 pt-24">
+    <div class="mr-20 pt-20">
       <slot/>
     </div>
   </div>
